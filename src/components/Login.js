bcce3e8923f4635ef7542ba399c8fa382ec0134f
arpitsinh-thakor <1803arpitthakor@gmail.com>
2023-10-21 01:35:04 +0530

    commit_4 authentication implementation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
-import { checkValidData } from "../utils/validate";
+import { checkValidData, checkValidData2 } from "../utils/validate";
+import { auth } from "../utils/firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -13,8 +16,42 @@ const Login = () => {
     setIsSignInForm(!isSignInForm);
   };
   const handleButtonClick = ()=>{
-    const message = checkValidData(email.current.value, password.current.value, name.current.value);
-    setErrorMessage(message);
+    
+      // if(message) return; //error
+    //sign in/ sign out
+    if(!isSignInForm){
+      //sign up logic
+        const message = checkValidData2(email.current.value, password.current.value, name.current.value);
+        setErrorMessage(message);
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+          .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode + " - " + errorMessage);
+          });
+
+    }
+    else{
+      const message = checkValidData(email.current.value, password.current.value);
+        setErrorMessage(message);
+      //sign in logic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + " - " + errorMessage);
+      });
+    }
+
   }
 
   return (
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,16 @@
-export const checkValidData = (email, password, name) => {
+export const checkValidData2 = (email, password, name) => {
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    const isName = /(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(name);
+  
+    if (!isEmailValid) return "Email ID is not valid";
+    if (!isPasswordValid) return "Password is not valid";
+    if(!isName) return "Name is not valid";
+  
+    return null;
+  };
+
+  export const checkValidData = (email, password, name) => {
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
     const isName = /(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(name);

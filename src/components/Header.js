223c17e6c165cd598991a63398192e5d55a21370
arpitsinh-thakor <1803arpitthakor@gmail.com>
2023-10-22 03:51:27 +0530

    commit_15 Bulding Video Background

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ function Header() {
       }, []);
 
   return (
-    <div className="absolute w-screen px-4 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-screen px-4 py-2 bg-gradient-to-b from-black z-10 flex justify-between bg-transparent">
         <img 
             className="w-44"
             src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,15 +2,15 @@
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-4xl font-bold">{title}</h1>
       <p className="py-6 w-1/4">{overview}</p>
       <div>
         <button 
-          className="bg-gray-500 text-black p-2 mx-2 px-10 text-lg bg-opacity-50 rounded-lg"
+          className="bg-white text-black p-2 mx-2 px-10 text-lg rounded-lg hover:bg-opacity-80"
           >Play</button>
         <button 
-          className="bg-gray-500 text-black p-2 px-10 text-lg bg-opacity-50 rounded-lg"
+          className="bg-gray-600 text-white p-2 px-10 text-lg  rounded-lg hover:bg-opacity-80"
           >More Info</button>
       </div>
     </div>

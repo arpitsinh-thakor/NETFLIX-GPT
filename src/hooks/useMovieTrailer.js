ddd97a296c6e19634a692c86674b9296afbbdac9
Thakor Arpitsinh <1803arpitthakor@gmail.com>
2026-08-28 22:33:37 +0530

    hooks edited

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,26 +1,41 @@
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrailerVideo } from '../utils/movieSlice';
-import { useEffect } from 'react';
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
+import { useCallback, useEffect } from "react";
 
-const useMovieTrailer = (movieId)=>{
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
 
-    const dispath = useDispatch();
+  const trailerVideo = useSelector(
+    (store) => store.movies?.trailerVideo
+  );
 
-    const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-    const getMovieVideos = async()=>{
-    const data = 
-        await 
-            fetch('https://api.themoviedb.org/3/movie/'+ movieId +'/videos?language=en-US', API_OPTIONS);
+  const getMovieVideos = useCallback(async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+
     const json = await data.json();
-    const filterData = json.results.filter(video => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0]: json.results[0];
-    dispath(addTrailerVideo(trailer));
-  };
 
-  useEffect(()=>{
-    if(!trailerVideo)
+    const filterData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+
+    const trailer = filterData.length
+      ? filterData[0]
+      : json.results[0];
+
+    dispatch(addTrailerVideo(trailer));
+  }, [movieId, dispatch]);
+
+  useEffect(() => {
+    if (!trailerVideo) {
       getMovieVideos();
-  }, []);
-}
+    }
+  }, [trailerVideo, getMovieVideos]);
+};
+
 export default useMovieTrailer;
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,24 +1,31 @@
-import {useEffect} from "react";
-import {API_OPTIONS} from '../utils/constants'
+import { useEffect, useCallback } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import {addNowPlayingMovies} from "../utils/movieSlice"
+import { addNowPlayingMovies } from "../utils/movieSlice";
 
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
 
-const useNowPlayingMovies = ()=>{
-    const dispath = useDispatch();
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
 
-    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies);
+  const getNowPlayingMovies = useCallback(async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
 
-    const getNowPlayingMovies = async () =>{
-      const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
-      const json = await data.json();
-      dispath(addNowPlayingMovies(json.results));
-    };
-  
-    useEffect(()=>{
-      if(!nowPlayingMovies)
-        getNowPlayingMovies();
-    }, []);
+    const json = await data.json();
+
+    dispatch(addNowPlayingMovies(json.results));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!nowPlayingMovies) {
+      getNowPlayingMovies();
+    }
+  }, [nowPlayingMovies, getNowPlayingMovies]);
 };
 
 export default useNowPlayingMovies;
